759c89046b7e235b59fe2758dcf98ada8af331ac
jina <trannnhii2407@gmail.com>
2025-06-18 23:03:30 +0800

    Refactor sign-up screen

--- app/src/main/java/com/example/project01_spotify/SignUp.java
+++ app/src/main/java/com/example/project01_spotify/SignUp.java
@@ -1,6 +1,9 @@
 package com.example.project01_spotify;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,13 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+        Button button = (Button) findViewById(R.id.loginClickButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the button");
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
